c89d1b0c316a9ea67b9ad22b04e182c844ee8bd2
1tc-selode <oseletye@gmail.com>
2026-02-24 10:05:02 +0100

    frontend nem mukodik

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 
+import { AuthGuard } from './core/guards/auth-guard';
+
 export const routes: Routes = [
 	{
 		path: '',
-		loadChildren: () => import('./features/reports/reports-module').then(m => m.ReportsModule)
+		loadChildren: () => import('./features/reports/reports-module').then(m => m.ReportsModule),
+		canActivate: [AuthGuard]
 	},
 	{
 		path: 'reports',
-		loadChildren: () => import('./features/reports/reports-module').then(m => m.ReportsModule)
+		loadChildren: () => import('./features/reports/reports-module').then(m => m.ReportsModule),
+		canActivate: [AuthGuard]
 	},
 	{
 		path: 'login',
@@ -19,14 +23,17 @@ export const routes: Routes = [
 	},
 	{
 		path: 'profile',
-		loadComponent: () => import('./features/profile/profile').then(m => m.ProfileComponent)
+		loadComponent: () => import('./features/profile/profile').then(m => m.ProfileComponent),
+		canActivate: [AuthGuard]
 	},
 	{
 		path: 'statistics',
-		loadChildren: () => import('./features/statistics/statistics-module').then(m => m.StatisticsModule)
+		loadChildren: () => import('./features/statistics/statistics-module').then(m => m.StatisticsModule),
+		canActivate: [AuthGuard]
 	},
 	{
 		path: 'admin',
-		loadChildren: () => import('./features/admin/admin-module').then(m => m.AdminModule)
+		loadChildren: () => import('./features/admin/admin-module').then(m => m.AdminModule),
+		canActivate: [AuthGuard]
 	}
 ];
